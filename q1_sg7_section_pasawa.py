3bb39222b58c8f9363e49a83a07dab044ccabb6e
desireepasawa <lagrimasdesiree5@gmail.com>
2026-09-24 06:40:28 +0800

    Add classes for Glassware, Beaker, and Tray

---
+++ q1_sg7_section_pasawa.py
@@ -0,0 +1,25 @@
+class Glassware:
+    pass
+
+class Beaker(Glassware):
+    pass
+
+class Tray:
+    def __init__(self):
+        self.beakers = [Beaker(), Beaker(), Beaker(), Beaker(), Beaker()]
+
+tray = Tray()
+
+print("5 beakers in the tray")class Glassware:
+    pass
+
+class Beaker(Glassware):
+    pass
+
+class Tray:
+    def __init__(self):
+        self.beakers = [Beaker(), Beaker(), Beaker(), Beaker(), Beaker()]
+
+tray = Tray()
+
+print("5 beakers in the tray")
